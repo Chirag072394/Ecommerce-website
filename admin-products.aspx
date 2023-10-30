635b3d26651a6a98b26244a26aba7a985ae3830b
Chirag <chirag7pandey@gmail.com>
2023-10-30 18:09:30 +0530

    admin products add apsx and database created

---
+++ admin-products.aspx
@@ -0,0 +1,203 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminMasterPage.master" AutoEventWireup="true" CodeFile="admin-products.aspx.cs" Inherits="adminproducts" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script type="text/javascript">
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+
+                var reader = new FileReader();
+                00
+                reader.onload = function (e) {
+                    $('.file-upload-image').attr('src', e.target.result);
+                    $('.image-upload-wrap').show();
+                    $('.image-title').html(input.files[0].name);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+
+            } else {
+                $('.image-upload-wrap').hide();
+            }
+        }
+        function removeUpload() {
+            $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+            $('.image-upload-wrap').hide();
+        }
+</script>
+    <style>
+
+     /*Image*/
+    .file-upload {
+		background-color: #ffffff;
+		width: 100%;
+		margin: 0 auto;
+		padding: 2px;
+	}
+
+	.file-upload-btn{
+		width: 100%;
+		padding: 1px;
+		text-transform: uppercase;
+		color: #fff;
+		background-color: #ff6800;
+		border: 2px solid #ff6800;
+	}
+	.file-upload-input{
+		display: none;
+	}
+
+	.image-upload-wrap {
+		margin-top: 2px;
+		border: 2px solid #ff6800;
+	}
+	.image-title-wrap {
+		padding: 1px;
+		background-color: #ff6800;
+		color: #ffffff; 
+	}
+
+	.file-upload-image{
+		display: block;
+        height:200px;
+		max-height: 200px;
+		width: 100%;
+        max-width:100%;
+		padding: 2px;
+	}
+
+	.remove-image {
+		float: right;
+		background-color: transparent;
+		border: 1px solid #ffffff;
+		color: #ffffff;
+		font-weight: 50;
+	}
+</style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+ 
+   <section id="admin-products">               
+    <div class="row">
+        <div class="col-md-12" style="height:60px;"></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading" style="color:#fff; font-size:16pt;">Products</div>
+                <div class="panel-body" style="background-color:white;">
+                    <div class="form-group">
+                        <label for="name">Product Name</label>
+                        <asp:TextBox id="txtproductname" runat="server" class="form-control"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtproductname" 
+                        ValidationGroup="vg1" ErrorMessage="Name can't be blank" ForeColor="Red" Display="Dynamic" ></asp:RequiredFieldValidator>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Category</label>
+                        <div class="form-group" style="text-align:left;">
+                    
+                    <asp:DropDownList ID="ddlcategory" runat="server" AutoPostBack="false" class="form-control">
+                        <asp:ListItem Text="Select">Select</asp:ListItem>
+                        <asp:ListItem>Men</asp:ListItem>
+                        <asp:ListItem>Women</asp:ListItem>
+                        <asp:ListItem>Kids</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" ControlToValidate="ddlcategory" 
+                        ValidationGroup="vg1" ErrorMessage="Category Name is required" InitialValue="Select" Display="Dynamic" ForeColor="Red">
+                    </asp:RequiredFieldValidator>
+                </div>
+                         <div class="form-group">
+                       <div class="image-upload-wrap">
+			                            <div class="image-title-wrap" style="display:none"><span class="image-title">Uploaded Image</span><button type="button" onclick="removeUpload()" class="remove-image">X</button></div>
+			                            <asp:Image id="apllicantimg" class="file-upload-image" runat="server" alt="Image"/>
+		                            </div>
+		                           <button class="file-upload-btn" type="button" onclick="$('.file-upload-input').trigger( 'click' )">Add Image</button>
+		                            <asp:FileUpload class="file-upload-input" ID="fileuploadimg" runat="server" type='file' onchange="readURL(this);" accept="image/*" style="display:none"/>
+                                    <asp:Label ID="lblmsg" runat="server" Text="Label" Visible="False"></asp:Label>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Price</label>
+                        <asp:TextBox id="txtprice" runat="server"  class="form-control"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtprice" 
+                        ValidationGroup="vg1" ErrorMessage="price can't blank" ForeColor="Red" Display="Dynamic"></asp:RequiredFieldValidator>
+                    </div>
+                     <div class="form-group">
+                        <label for="name">Quantity</label>
+                        <asp:TextBox id="txtquantity" runat="server"  class="form-control"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="txtquantity" 
+                        ValidationGroup="vg1" ErrorMessage="qunatity can't blank" ForeColor="Red" Display="Dynamic"></asp:RequiredFieldValidator>
+                    </div>
+                    <div class="form-group" style="text-align:center;">
+                        <asp:Button ID="btnadd" class="btn btn-primary btn-lg" causevalidation="true"  runat="server" Text="ADD" />
+                        <asp:Button ID="btnsave" class="btn btn-primary btn-lg" causesvalidation="true" ValidationGroup="vg1" runat="server"  Text="Save" />
+                        <asp:Button ID="btncancel" class="btn btn-primary btn-lg" causesvalidation="true" runat="server" Text="Cancel" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>  
+   <%-- <div class="col-md-12">
+        <div class="form-group" style="text-align:center;">
+            <asp:CheckBox ID="chksearch" runat="server" Text="Search Admission Enquiry By Name:" Checked="false"/>
+            <asp:TextBox ID="txtsearch" runat="server" Class="form-control"/><br />
+            <asp:Button ID="btnsearch" runat="server" Text="Search" OnClick="RecordSearch" Class="btn btn-primary btn-lg" />
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True"  style="margin-top:20px; margin-left:0px; width:100%;"
+            AllowSorting="True" AutoGenerateColumns="false"  AutoGenerateEditButton="false" 
+            CellPadding="4" DataKeyNames="id" ForeColor="#333333" GridLines="None" 
+            PageSize="25" Height="40px" RowStyle-Height="25" 
+            OnRowCommand="GridView1_RowCommand1" BorderColor="#CCCCCC" BorderStyle="Solid" 
+            BorderWidth="1px" onpageindexchanging="GridView1_PageIndexChanging" 
+            onrowdeleting="GridView1_RowDeleting">
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <RowStyle BackColor="#EFF3FB" />
+            <EditRowStyle BackColor="#2FF1BF" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" Height="18px" Font-Size="10pt" HorizontalAlign="Center"/>
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:TemplateField HeaderText="Id" Visible="false">
+                    <ItemTemplate> 
+                        <%# Eval("id") %>
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="left" Width="0%"></ItemStyle>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Name">
+                    <ItemTemplate>
+                        <%# Eval("name") %>
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="left" Width="20%"></ItemStyle>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Email">
+                    <ItemTemplate>
+                        <%# Eval("email") %>
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="left" Width="15%"></ItemStyle>
+                </asp:TemplateField>
+                
+                <asp:TemplateField HeaderText="Mobile No.">
+                    <ItemTemplate>
+                        <%#Eval("mobile") %>
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="left" Width="10%"></ItemStyle>
+                </asp:TemplateField>
+    
+                <asp:TemplateField HeaderText="Edit">
+                    <ItemTemplate> 
+                        <asp:LinkButton ID="lbED" runat="server" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "id") %>'  CommandName="EditData"   Text="Edit"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Delete?">
+                    <ItemTemplate>
+                        <span onclick="return confirm('Are you sure to Delete?')">
+                        <asp:LinkButton ID="lnBD" runat="server" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "id") %>' CommandName="Delete" Text="Delete"></asp:LinkButton>
+                        </span>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>--%>
+    </div>     
+</section>
+</asp:Content>
+
---
+++ admin-products.aspx.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.IO;
+
+
+public partial class adminproducts : System.Web.UI.Page
+{
+    SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["myConn"].ConnectionString);
+    SqlCommand cmd = new SqlCommand();
+    public string sql = null;
+
+    public class global
+    {
+        public static bool NewRecords = false;
+        public static bool EditRecords = true;
+        public static bool DeleteRecords = true;
+        public static int id;
+        public static bool SaveRecord = false;
+        public static bool CancelRecords { get; set; }
+    }
+
+
+    //protected void Page_Load(object sender, EventArgs e)
+    //{
+    //    if (!IsPostBack)
+    //    {
+    //        GetData();
+    //        global.EditRecords = false;
+    //        global.NewRecords = true;
+    //        btnadd.Visible = true;
+    //        btnsave.Visible = false;
+    //        btncancel.Visible = false;
+    //        txtsearch.Text = "";
+    //    }
+    //}
+    //private void GetData()
+    //{
+    //    conn.Open();
+    //    SqlCommand cmd = new SqlCommand("select * from products order by id desc", conn);
+    //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+    //    DataSet ds = new DataSet();
+    //    da.Fill(ds);
+    //    GridView1.DataSource = ds.Tables[0];
+    //    GridView1.DataBind();
+    //    conn.Close();
+    //}
+    //protected void RecordNew(object sender, EventArgs e)
+    //{
+    //    global.EditRecords = false;
+    //    global.NewRecords = true;
+    //    btnadd.Visible = false;
+    //    btnsave.Visible = true;
+    //    btncancel.Visible = true;
+    //    chksearch.Checked = false;
+    //    BlankRecord();
+    //}
+
+    //protected void RecordSave(object sender, EventArgs e)
+    //{
+    //    if (global.EditRecords == true)
+    //    {
+    //        UpdateRecord();
+    //        global.EditRecords = false;
+    //        global.NewRecords = false;
+    //        btnnew.Visible = true;
+    //        btnsave.Visible = true;
+    //        btncancel.Visible = false;
+    //        chksearch.Checked = false;
+    //        BlankRecord();
+    //        GridView1.EditIndex = -1;
+    //        GetData();
+    //    }
+    //    else if (global.NewRecords == true)
+    //    {
+    //        AddRecord();
+    //        global.EditRecords = false;
+    //        global.NewRecords = true;
+    //        btnnew.Visible = false;
+    //        btncancel.Visible = true;
+    //        btnsave.Visible = true;
+    //        chksearch.Checked = false;
+    //        global.SaveRecord = true;
+    //        BlankRecord();
+    //        GridView1.EditIndex = -1;
+    //        GetData();
+    //        if (global.SaveRecord == true)
+    //        {
+    //            btnnew.Visible = true;
+    //            btnsave.Visible = false;
+    //            btncancel.Visible = false;
+    //            txtsearch.Text = "";
+
+    //        }
+    //    }
+
+    //}
+
+    //private void UpdateRecord()
+    //{
+    //    try
+    //    {
+    //        if (global.EditRecords == true)
+    //        {
+    //            global.NewRecords = false;
+    //            string vname = txtproductname.Text.Trim();
+    //            string vcategory= txtcategory.Text.Trim();
+    //            string vprice = txtprice.Text.Trim();
+    //            string vquantity = txtquantity.Text.Trim();
+    //            conn.Open();
+    //            sql = "update  set name=@vvname,mobile=@vvmobile,email=@vvemail where id= @vvid";
+    //            SqlCommand cmd = new SqlCommand(sql, conn);
+    //            cmd.Parameters.AddWithValue("@vvid", global.id);
+    //            cmd.Parameters.AddWithValue("@vvname", vname);
+    //            cmd.Parameters.AddWithValue("@vvmobile", vprice);
+    //            cmd.Parameters.AddWithValue("@vvemail", vquantity);
+    //            cmd.ExecuteNonQuery();
+    //            conn.Close();
+    //            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Updated Successfully')", true);
+    //        }
+    //    }
+    //    catch (Exception ee)
+    //    {
+    //        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(ee.Message.ToString())", true);
+    //    }
+    //}
+
+    //public void AddRecord()
+    //{
+    //    try
+    //    {
+    //        if (global.NewRecords == true)
+    //        {
+    //            string vname = txtname.Text.Trim();
+    //            string vmobile = txtmobile.Text.Trim();
+    //            string vEmail = txtemail.Text.Trim();
+    //               fileupload();
+    //               string vimg = s;
+    //            conn.Open();
+    //            sql = "insert into enquiry(name,mobile,email) values(@vvname,@vvmobile,@vvemail)";
+    //            SqlCommand cmd = new SqlCommand(sql, conn);
+    //            cmd.Parameters.AddWithValue("@vvname", vname);
+    //            cmd.Parameters.AddWithValue("@vvmobile", vmobile);
+    //            cmd.Parameters.AddWithValue("@vvemail", vEmail);
+    //            cmd.ExecuteNonQuery();
+    //            conn.Close();
+    //            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Save Successfully')", true);
+    //        }
+    //    }
+    //    catch (Exception ee)
+    //    {
+    //        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(ee.Message.ToString())", true);
+    //    }
+    //}
+
+    //private void fileupload()
+    //{
+    //    string fileName = string.Empty;
+    //    string extension = string.Empty;
+    //    try
+    //    {
+    //        if (fileuploadimg.HasFile)
+    //        {
+    //            extension = Path.GetExtension(fileuploadimg.FileName).ToLower();
+    //            if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif" || extension == ".bmp")
+    //            {
+    //                s = @"~/images/products/" + fileuploadimg.FileName;
+    //                fileuploadimg.PostedFile.SaveAs(Server.MapPath(s));
+    //            }
+    //            else
+    //            {
+    //                lblmsg.Visible = false;
+    //                lblmsg.Text = "Please select jpg, jpeg, png, gif or bmp file only";
+    //            }
+    //        }
+    //        else
+    //        {
+    //            lblmsg.Visible = false;
+    //            lblmsg.Text = "Please select file to upload";
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        lblmsg.Visible = false;
+    //        lblmsg.Text = "Oops!! error occured : " + ex.Message.ToString();
+    //    }
+
+    //}
+
+    //protected void RecordCancel(object sender, EventArgs e)
+    //{
+    //    global.EditRecords = false;
+    //    global.NewRecords = false;
+    //    btnnew.Visible = true;
+    //    btnsave.Visible = false;
+    //    btncancel.Visible = false;
+    //    chksearch.Checked = false;
+    //    BlankRecord();
+    //    GridView1.EditIndex = -1;
+    //    GetData();
+    //}
+
+    //protected void GridView1_RowCommand1(object sender, GridViewCommandEventArgs e)// for grids edit and delete button
+    //{
+    //    if (e.CommandName == "EditData")
+    //    {
+    //        global.EditRecords = true;
+    //        global.DeleteRecords = false;
+    //        btnnew.Visible = false;
+    //        btnsave.Visible = true;
+    //        btncancel.Visible = true;
+    //        chksearch.Checked = false;
+    //        global.id = Convert.ToInt32(e.CommandArgument);
+    //        using (var connection = conn)
+    //        using (var command = connection.CreateCommand())
+    //        {
+    //            command.CommandText = "Select * from products where id=" + global.id + "";
+    //            connection.Open();
+    //            using (var reader = command.ExecuteReader())
+    //            {
+    //                while (reader.Read())
+    //                {
+    //                    txtname.Text = reader["name"].ToString();
+    //                    txtmobile.Text = reader["mobile"].ToString();
+    //                    txtemail.Text = reader["email"].ToString();
+
+    //                }
+    //            }
+    //            connection.Close();
+    //        }
+    //    }
+    //    if (e.CommandName == "Delete")
+    //    {
+    //        btnnew.Visible = true;
+    //        chksearch.Checked = false;
+    //        global.id = Convert.ToInt32(e.CommandArgument);
+    //        conn.Open();
+    //        SqlCommand cmd = new SqlCommand("delete from products where id=" + global.id + "", conn);
+    //        cmd.ExecuteNonQuery();
+    //        conn.Close();
+    //        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Deleted Successfully')", true);
+    //        GetData();
+    //    }
+    //}
+
+    //protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    //{
+    //    chksearch.Checked = false;
+    //    GetData();
+    //}
+
+    //protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    //{
+    //    chksearch.Checked = false;
+    //    GridView1.PageIndex = e.NewPageIndex;
+    //    GetData();
+    //}
+
+    //public void BlankRecord()
+    //{
+    //    txtname.Text = "";
+    //    txtmobile.Text = "";
+    //    txtemail.Text = "";
+    //}
+
+    //protected void RecordSearch(object sender, EventArgs e)
+    //{
+    //    if (chksearch.Checked == true)
+    //    {
+    //        GetDataSearch();
+    //    }
+    //}
+
+    //private void GetDataSearch()
+    //{
+    //    SqlCommand cmd = new SqlCommand("select * from products where name like '" + txtsearch.Text + "%' order by id desc", conn);
+    //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+    //    DataSet ds = new DataSet();
+    //    da.Fill(ds);
+    //    if (ds.Tables[0].Rows.Count == 0)
+    //    {
+    //        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Sorry! Please Enter Correct Name')", true);
+    //    }
+    //    else
+    //    {
+    //        GridView1.DataSource = ds.Tables[0];
+    //        GridView1.DataBind();
+    //    }
+    //    conn.Close();
+    //}
+
+
+
+}
